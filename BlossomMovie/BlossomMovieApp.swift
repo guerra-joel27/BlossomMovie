194255f7f74a24819d9596d62856da379850edb8
Joel Guerra <joel.guerra@mavs.uta.edu>
2026-08-22 18:36:59 -0500

    Initial Commit

---
+++ BlossomMovie/BlossomMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomMovieApp.swift
+//  BlossomMovie
+//
+//  Created by Joel Guerra on 8/22/26.
+//
+
+import SwiftUI
+
+@main
+struct BlossomMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlossomMovie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlossomMovie
+//
+//  Created by Joel Guerra on 8/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
