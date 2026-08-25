a2b33b9ffd1262bf4b432b54d6c408e9278ef3a9
Joel Guerra <guerra.joel27@gmail.com>
2026-08-24 21:51:25 -0500

    ContentView setup and starting to add API integration

---
+++ BlossomMovie/Title.swift
@@ -0,0 +1,19 @@
+//
+//  Title.swift
+//  BlossomMovie
+//
+//  Created by Joel Guerra on 8/24/26.
+//
+
+import Foundation
+
+struct APIObject: Decodable {
+    var results: [Title] = []
+}
+
+struct Title: Decodable, Identifiable {
+    var id: Int
+    var title: String
+    var overview: String
+    var posterPath: String
+}
