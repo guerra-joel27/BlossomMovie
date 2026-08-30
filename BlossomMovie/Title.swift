e4f26a2cffe7fad6c43a7ea967452009ac72123c
Joel Guerra <guerra.joel27@gmail.com>
2026-08-29 21:32:52 -0500

    changed variables in Struct to optionals

--- BlossomMovie/Title.swift
+++ BlossomMovie/Title.swift
@@ -12,8 +12,9 @@ struct APIObject: Decodable {
 }
 
 struct Title: Decodable, Identifiable {
-    var id: Int
-    var title: String
-    var overview: String
-    var posterPath: String
+    var id: Int?
+    var title: String?
+    var name: String?
+    var overview: String?
+    var posterPath: String?
 }
