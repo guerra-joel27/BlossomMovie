d4bcd04c9bdda6f1691a9727b0f9ddc5194a0414
Joel Guerra <guerra.joel27@gmail.com>
2026-09-07 22:43:55 -0500

    added the beginning stages of View Model

---
+++ BlossomMovie/ViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  ViewModel.swift
+//  BlossomMovie
+//
+//  Created by Joel Guerra on 9/7/26.
+//
+
+import Foundation
+
+@Observable
+class ViewModel {
+    enum FetchStatus {
+        case notStarted
+        case fetching
+        case success
+        case failed(underlyingError: Error)
+    }
+    private(set) var homeStatus: FetchStatus = .notStarted
+    private let dataFetcher = DataFetcher()
+    var trendingMovies: [Title] = []
+    
+    func getTitle() async {
+        homeStatus = .fetching
+        
+        do {
+            trendingMovies = try await dataFetcher.fetchTitles(for: "movie")
+            homeStatus = .success
+        } catch {
+            print(error)
+            homeStatus = .failed(underlyingError: error)
+        }
+    }
+}
