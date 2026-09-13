8c913beb5dd20eee79b0f5052b05ed0654d172e2
Joel Guerra <guerra.joel27@gmail.com>
2026-09-12 23:55:46 -0500

    notes

--- BlossomMovie/BlossomMovieApp.swift
+++ BlossomMovie/BlossomMovieApp.swift
@@ -5,6 +5,10 @@
 //  Created by Joel Guerra on 8/22/26.
 //
 
+// this is like the beginning of the application
+// everything is built from here. we just have the bare minimum kinda here
+// and everything else is build from components from here
+
 import SwiftUI
 
 @main
