370c69c14b604320b40c987767e8f93c049dc94d
Joel Guerra <guerra.joel27@gmail.com>
2026-09-13 21:57:04 -0500

    added retrieval for the other three sections we need

--- BlossomMovie/HomeView.swift
+++ BlossomMovie/HomeView.swift
@@ -52,9 +52,9 @@ struct HomeView: View {
                             }
                         }
                         HorizontalListView(header: Constants.trendingMovieString, titles: viewModel.trendingMovies)
-    //                    HorizontalListView(header: Constants.trendingTVString)
-    //                    HorizontalListView(header: Constants.topRatedMovieString)
-    //                    HorizontalListView(header: Constants.topRatedTVString)
+                        HorizontalListView(header: Constants.trendingTVString, titles: viewModel.trendingTV)
+                        HorizontalListView(header: Constants.topRatedMovieString, titles: viewModel.topRatedMovies)
+                        HorizontalListView(header: Constants.topRatedTVString, titles: viewModel.topRatedTV)
                     }
                 case .failed(let error):
                     Text("Error: \(error.localizedDescription)")
--- BlossomMovie/ViewModel.swift
+++ BlossomMovie/ViewModel.swift
@@ -18,12 +18,18 @@ class ViewModel {
     private(set) var homeStatus: FetchStatus = .notStarted
     private let dataFetcher = DataFetcher()
     var trendingMovies: [Title] = []
+    var trendingTV: [Title] = []
+    var topRatedMovies: [Title] = []
+    var topRatedTV: [Title] = []
     
     func getTitles() async {
         homeStatus = .fetching
         
         do {
             trendingMovies = try await dataFetcher.fetchTitles(for: "movie", by: "trending")
+            trendingTV = try await dataFetcher.fetchTitles(for: "tv", by: "trending")
+            topRatedMovies = try await dataFetcher.fetchTitles(for: "movie", by: "top_rated")
+            topRatedTV = try await dataFetcher.fetchTitles(for: "tv", by: "top_rated")
             homeStatus = .success
         } catch {
             print(error)
