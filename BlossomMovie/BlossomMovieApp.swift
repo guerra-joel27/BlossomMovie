ffcef4a072ee8e1f5476ef17dbc6f6725cc45233
Joel Guerra <guerra.joel27@gmail.com>
2026-09-25 00:42:17 -0500

    minor changes

--- BlossomMovie/BlossomMovieApp.swift
+++ BlossomMovie/BlossomMovieApp.swift
@@ -7,7 +7,7 @@
 
 // this is like the beginning of the application
 // everything is built from here. we just have the bare minimum kinda here
-// and everything else is build from components from here
+// and everything else is build from components from here, nice!->
 
 import SwiftUI
 
